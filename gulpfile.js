45f673b92b8fca63dce4ae5a04f6b40c76b3b3e9
Yan Zhu <picturepan2@hotmail.com>
2019-03-15 16:19:10 +0800

    Add YAML data processing

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,24 @@ var autoprefixer = require('gulp-autoprefixer');
 var pug = require('gulp-pug');
 var data = require('gulp-data');
 var plumber = require('gulp-plumber');
+var yaml = require('gulp-yaml');
 var fs = require('fs');
 
 var paths = {
   scss: './scss/*.scss',
+  data: './data/*.yml',
   pug: './pug/!(_)*.pug'
 };
 
 gulp.task('watch', function() {
   gulp.watch('./**/*.scss', ['build']);
   gulp.watch('./**/*.pug', ['web']);
+});
+
+gulp.task('data', function() {
+  gulp.src(paths.data)
+    .pipe(yaml({ schema: 'DEFAULT_SAFE_SCHEMA' }))
+    .pipe(gulp.dest('./data/'))
 });
 
 gulp.task('build', function() {
