aea4a45cadef23e2f7985e5589f2993929875ca4
Yan Zhu <picturepan2@hotmail.com>
2019-03-19 17:33:58 +0800

    Update date

--- utilities/convert.js
+++ utilities/convert.js
@@ -1,15 +1,18 @@
 const yaml = require('yaml');
 const md5 = require('md5');
+const date = require('date-and-time');
 const fs = require('fs');
 
 async function run() {
   const file = fs.readFileSync('./data/blockevent.yml', 'utf8');
 
   var results = yaml.parse(file);
-  //console.log(results);
 
   for (var i = 0 in results) {
     results[i].hash = md5(results[i].name);
+    var datetime = new Date(results[i].date.start);
+    var dtstamp = date.addDays(datetime, -1);
+    results[i].date.dtstamp = dtstamp;
     console.log(results[i]);
   }
 
